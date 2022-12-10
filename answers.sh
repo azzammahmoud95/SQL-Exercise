a5586fd09b2f9e0b972f17408f0065265dd96d9c
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-10 22:30:29 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+'''SELECT name FROM students''' --Answer-1 
+'''SELECT * FROM students WHERE Age>30;''' --Answer-2
+'''SELECT name FROM students WHERE Age>30 AND Gender="F"''' --Answer-3
+'''SELECT Points FROM students WHERE name="Alex"''' --Answer-4
+'''INSERT INTO students(ID,name,Age,Gender,Points) VALUES("7","Azzam","21","M","900")''' --Answer-5
+'''UPDATE students SET Points="800" WHERE name="Basma"''' --Answer-6
+'''UPDATE students SET Points="100" WHERE name="Alex"''' --Answer-7
