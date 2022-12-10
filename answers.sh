252d40b8183eab37d78863c62d95c01d15970648
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-10 23:35:15 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,8 @@
 '''INSERT INTO students(ID,name,Age,Gender,Points) VALUES("7","Azzam","21","M","900")''' --Answer-5
 '''UPDATE students SET Points="800" WHERE name="Basma"''' --Answer-6
 '''UPDATE students SET Points="100" WHERE name="Alex"''' --Answer-7
+
+
+'''CREATE TABLE  graduates(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL UNIQUE, Age INTEGER , Gender TEXT, Points INTEGER, graduation TEXT );'''
+'''INSERT INTO graduates (ID,name,Age,Gender,Points,graduation) VALUES("4","Layal","18","F","350","08/09/2018")''' --Answer-11 and 10
+'''DELETE  FROM students WHERE name="Layal"''' --Answer-12
